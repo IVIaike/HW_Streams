2c911bcfb0b587a795eba931f74bab822866e0b6
IVIaike <trainer.ekb@gmail.com>
2023-02-16 01:30:15 +0500

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stream<String> streamOf = Stream.of("a", "b", "c", "d", "z", "xy", "xv", "yz");
+
+    }
+}
